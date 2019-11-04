c11da9fcdb56e8898a33e3b3e4c5bcf561cdef14
Jakub <jakub.dobrowolski@besidethepark.com>
2019-11-04 16:50:36 +0100

    Updates

---
+++ precache-manifest.4f55fa108799b08364fa2f95e6c710e4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb7d847172fee462580e59d14300db7",
+    "url": "/gif-react-app/index.html"
+  },
+  {
+    "revision": "153b313b1f87b954d890",
+    "url": "/gif-react-app/static/css/main.47771816.chunk.css"
+  },
+  {
+    "revision": "34e5c5815f1cb1ce9201",
+    "url": "/gif-react-app/static/js/2.d1a86030.chunk.js"
+  },
+  {
+    "revision": "153b313b1f87b954d890",
+    "url": "/gif-react-app/static/js/main.882508bf.chunk.js"
+  },
+  {
+    "revision": "3e07e33966f64ad259de",
+    "url": "/gif-react-app/static/js/runtime-main.c87d208a.js"
+  },
+  {
+    "revision": "085414edc3320d73947fc4467e94b790",
+    "url": "/gif-react-app/static/media/Oswald-Light.085414ed.ttf"
+  },
+  {
+    "revision": "c76a8d044f123462ba5c08ea47b56e21",
+    "url": "/gif-react-app/static/media/Oswald-Medium.c76a8d04.ttf"
+  },
+  {
+    "revision": "077e87c6c30f988a8cd225faa83daf18",
+    "url": "/gif-react-app/static/media/Oswald-Regular.077e87c6.ttf"
+  },
+  {
+    "revision": "0cd19b8387ed144a3a37e48868c73f6b",
+    "url": "/gif-react-app/static/media/YeonSung-Regular.0cd19b83.ttf"
+  },
+  {
+    "revision": "4e0487c232582955f87fb88fcd6437d8",
+    "url": "/gif-react-app/static/media/logo.4e0487c2.svg"
+  },
+  {
+    "revision": "09b773495df08c18c7064d7721770408",
+    "url": "/gif-react-app/static/media/placeholder.09b77349.jpg"
+  }
+]);
